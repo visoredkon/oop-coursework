01a8b405067df52ef8aad871708e3fab3c01593c
Pahril Dwi Saputra <11221056@student.itk.ac.id>
2023-03-31 09:46:44 +0000

    Relocate main function to App.java

--- week-6/Academia/src/my/coursework/academia/App.java
+++ week-6/Academia/src/my/coursework/academia/App.java
@@ -1,7 +1,25 @@
 package my.coursework.academia;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    // Method main untuk menjalankan program
+    public static void main(String[] args) {
+        // Membuat objek person1 dengan konstruktor ke-2
+        Person person1 = new Person("Pahril", "Jl. Imus Payau");
+        // Membuat objek person2 dengan konstruktor ke-3
+        Person person2 = new Person("Saputra");
+
+        System.out.print("\033[H\033[2J");
+
+        System.out.println("==== PERSON DATA ====");
+        System.out.println("Nama    : " + person1.getName());
+        System.out.println("Alamat  : " + person1.getAddress());
+        System.out.println("=====================\n");
+
+        person2.setAddress("Kilometer 1");
+
+        System.out.println("==== PERSON 2 DATA ====");
+        System.out.println("Nama    : " + person2.getName());
+        System.out.println("Alamat  : " + person2.getAddress());
+        System.out.println("=======================\n");
     }
 }
